a9f801f1262756f3a390ba1f6da62c9ad05863d8
Max Maddux <maxmaddux@gmail.com>
2023-11-24 06:23:03 -0800

    Complete project

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-446be.firebaseapp.com",
-  projectId: "mern-estate-446be",
-  storageBucket: "mern-estate-446be.appspot.com",
-  messagingSenderId: "84945005638",
-  appId: "1:84945005638:web:9a599e80da6060d014ee47",
+  authDomain: 'mern-estate.firebaseapp.com',
+  projectId: 'mern-estate',
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: '1078482850952',
+  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
 };
 
 // Initialize Firebase
